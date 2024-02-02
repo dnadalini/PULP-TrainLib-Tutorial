e8eaaf09f6565a951e84b0373f844932546a9c79
Davide Nadalini <93909427+dnadalini@users.noreply.github.com>
2024-02-02 12:54:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-USR_PATH=$(pwd)/v1.0.16-pulp-riscv-gcc-ubuntu-18/
+USR_PATH=$(pwd)/v1.0.16-pulp-riscv-gcc-ubuntu-18
 export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
 export PATH=$USR_PATH/bin:$PATH
 source v1.0.16-pulp-riscv-gcc-ubuntu-18/sourceme.sh
