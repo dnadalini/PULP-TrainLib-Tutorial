91f9167de16685738f2177b3127f8cc9f82cd871
Davide Nadalini <93909427+dnadalini@users.noreply.github.com>
2024-02-02 12:50:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-export PULP_RISCV_GCC_TOOLCHAIN=v1.0.16-pulp-riscv-gcc-ubuntu-18/
+USR_PATH=$(pwd)/v1.0.16-pulp-riscv-gcc-ubuntu-18/
+export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
+export PATH=$USR_PATH/bin:$PATH
 source v1.0.16-pulp-riscv-gcc-ubuntu-18/sourceme.sh
 source pulp-sdk/configs/pulp-open.sh
 
