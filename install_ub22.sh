a7e3920e112b1c758f8f6e7cc297b004d6a20ee3
Davide Nadalini <d.nadalini@unibo.it>
2024-02-02 17:20:08 +0100

    Fix gitignore and partially fix installation for Ubuntu20

--- install_ub22.sh
+++ install_ub22.sh
@@ -7,9 +7,20 @@ sudo apt-get install -y build-essential git libftdi-dev libftdi1 doxygen python3
 pip install --user argcomplete pyelftools
 
 # Download gnu-gcc-toolchain
-cp resources/pulp-riscv-gnu-toolchain.tar.gz ./
-tar -xvf pulp-riscv-gnu-toolchain.tar.gz -C ./
-rm pulp-riscv-gnu-toolchain.tar.gz
+cp resources/pulp-riscv-gcc-toolchain-ubuntu22.tar.gz ./
+tar -xvf pulp-riscv-gcc-toolchain-ubuntu22.tar.gz -C ./
+rm pulp-riscv-gcc-toolchain-ubuntu22.tar.gz
+
+# Set up GCC version 9.4.0
+sudo apt-get update -y && \
+sudo apt-get upgrade -y && \
+sudo apt-get dist-upgrade -y && \
+sudo apt-get install build-essential software-properties-common -y && \
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+sudo apt-get update -y && \
+sudo apt-get install gcc-9 g++-9 -y && \
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9 && \
+sudo update-alternatives --config gcc
 
 # Build pulp-sdk
 cd pulp-riscv-gcc-toolchain
