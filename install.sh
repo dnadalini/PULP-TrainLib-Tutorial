329dd335023fea6d384dc9e8ea3fbe5730a2842c
Davide Nadalini <93909427+dnadalini@users.noreply.github.com>
2024-02-02 12:46:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ source sourceme.sh
 cd ..
 USR_PATH=$(pwd)/v1.0.16-pulp-riscv-gcc-ubuntu-18/
 export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
+export PATH=$USR_PATH/bin:$PATH
 cd pulp-sdk/
 source configs/pulp-open.sh
 make build
